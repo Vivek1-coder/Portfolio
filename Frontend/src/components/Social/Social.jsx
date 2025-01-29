08a9cb028392e702333c1e2dc0027c864927427b
Vivek <vy212205@gmail.com>
2025-01-29 10:52:05 +0530

    refine Navbar hamburger animation and update Social component text

--- Frontend/src/components/Social/Social.jsx
+++ Frontend/src/components/Social/Social.jsx
@@ -6,7 +6,7 @@ import linkedIn from '../../assets/images/linkedIn.jpeg'
 const Social = () => {
   return (
     <div className='flex gap-2 bg-white w-full md:w-88  h-auto text-black justify-center items-center px-5 rounded-2xl m-2 md:m-5'>
-      <p className='md:font-semibold'>Follow me on : </p>
+      <p className='md:font-semibold'>Follow me : </p>
       <a href="https://linkedin.com/in/vivek1-coder/"><img className='h-5 md:h-10' src={linkedIn} alt="" /></a>
       <a href="https://github.com/Vivek1-coder"><img  className='h-5 md:h-10' src={gthb} alt="" /></a>
       <a href="https://www.instagram.com/viv.ek_18055/"><img  className='h-5 md:h-10' src={insta} alt="" /></a>
