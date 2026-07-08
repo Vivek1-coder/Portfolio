318ac0bebf5e19219cb62d4f0d40ebc3b28cbf42
Vivek <vy212205@gmail.com>
2026-07-09 01:38:49 +0530

    resume update

--- Frontend/src/components/Buttons/DownloadCV.jsx
+++ Frontend/src/components/Buttons/DownloadCV.jsx
@@ -6,7 +6,7 @@ const DownloadCVButton = () => {
   return (
 
       <a
-        href="/Resume(off).pdf"
+        href="/resume.pdf"
         download
         className="relative flex gap-4 px-10 py-4 text-white font-semibold text-lg rounded-xl 
                    bg-slate-800/30 backdrop-blur-md border border-transparent 
